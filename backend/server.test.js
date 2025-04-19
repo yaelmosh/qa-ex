e86b78262354cd64870c53af581043cb94e5f33c
yaelmosh <yaelmo30@gmail.com>
2025-04-19 17:07:40 +0300

    added backend task update test
    tests no longer run in parallel. parallel causes race conditions because of server architecture

--- backend/server.test.js
+++ backend/server.test.js
@@ -1,6 +1,6 @@
 const SERVER_API_URL = 'http://localhost:3000';
 
-async function fetchApi(url, method = 'GET', body = null) {
+async function fetchApi(url, method = 'GET', body = null, shouldParseResponse = true) {
     const response = await fetch(`${SERVER_API_URL}${url}`, {
         method,
         body: body != null ? JSON.stringify(body) : null,
@@ -10,7 +10,9 @@ async function fetchApi(url, method = 'GET', body = null) {
         },
     });
 
-    return await response.json();
+    if (shouldParseResponse) {
+        return await response.json();
+    }
 }
 
 describe('api', () => {
@@ -31,5 +33,35 @@ describe('api', () => {
         expect(createdTask.description).toEqual(newTask.description);
         expect(createdTask.priority).toEqual(newTask.priority);
         expect(createdTask.dueDate).toEqual(newTask.dueDate);
+    })
+
+    test('should update task', async () => {
+        //creating the task that will be deleted
+        const oldTask = {
+            title: 'old task',
+            description: 'to do this important task',
+            priority: 'high',
+            dueDate: '2029-04-01',
+        }
+        const newTask = {
+            completed: true,
+            title: 'new task',
+            description: 'to do this important task',
+            priority: 'low',
+            dueDate: '2030-04-01',
+        }
+
+        const {id} = await fetchApi('/api/tasks', 'POST', oldTask);
+        await fetchApi(`/api/tasks/${id}`, 'PUT', newTask);
+        const allTasks = await fetchApi('/api/tasks');
+        const updatedTask = allTasks.find(task => task.id === id);
+
+        expect(updatedTask).not.toBeNull();
+        expect(updatedTask.title).toEqual(newTask.title);
+        expect(updatedTask.description).toEqual(newTask.description);
+        expect(updatedTask.priority).toEqual(newTask.priority);
+        expect(updatedTask.dueDate).toEqual(newTask.dueDate);
+    })
+
     })
 });
